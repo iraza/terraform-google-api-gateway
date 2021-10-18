ee7928ebdca0107a42a5d546c699cb008cda5ad6
Imam Raza <>
2021-10-18 12:33:46 +0500

    terraform api gateway module

---
+++ variable.tf
@@ -0,0 +1,21 @@
+variable "api_id" {
+  type = string
+
+}
+variable "api_config_id" {
+  type = string
+}
+variable "api_gateway_id" {
+  type = string
+}
+
+variable "open_api_document_filename" {
+  type = string
+}
+
+variable "open_api_document_file_path" {
+  type = string
+}
+variable "api_gateway_display_name" {
+  type = string
+}
